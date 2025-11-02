42785d61664e97ada3d8b51445688c34890dc79e
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-11-02 09:22:15 +0100

    back end : update useMapper

--- jwt/springboot/src/main/java/com/example/jwt/mapper/UserMapper.java
+++ jwt/springboot/src/main/java/com/example/jwt/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package com.example.jwt.mapper;
 
+import com.example.jwt.dto.ConnectedUserDTO;
 import com.example.jwt.dto.UserDTO;
+import com.example.jwt.dto.UserRecord;
 import com.example.jwt.entity.UserEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDTO entityToDTO(UserEntity userEntity);
+    ConnectedUserDTO entityToConnectedDTO(UserEntity userEntity);
+    UserEntity recordToEntity(UserRecord userRecord);
 }
