1100bac167fbfab95cb3f151743ae4a021dd4781
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-11-04 11:10:12 +0100

    back end : move config to a folder

--- jwt/springboot/src/main/java/com/example/jwt/config/DBConfig.java
+++ jwt/springboot/src/main/java/com/example/jwt/config/DBConfig.java
@@ -1,4 +1,4 @@
-package com.example.jwt;
+package com.example.jwt.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
